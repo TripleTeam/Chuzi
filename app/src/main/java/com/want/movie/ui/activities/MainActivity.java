f5b39afba9dd3f8f19d5bcf304ca36066ff77d2c
Kirill Makarov <kir-maka@yandex.ru>
2017-11-25 17:35:43 +0300

    move main activity to ui.activities

--- app/src/main/java/com/want/movie/ui/activities/MainActivity.java
+++ app/src/main/java/com/want/movie/ui/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.want.movie;
+package com.want.movie.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.want.movie.R;
 
 public class MainActivity extends AppCompatActivity {
 
