f1f0442e14724ce063a898c32a04073927276198
Kirill Makarov <kir-maka@yandex.ru>
2017-11-26 08:57:50 +0300

    add round view

---
+++ app/src/main/java/com/want/movie/ui/util/RoundedBitmapView.java
@@ -0,0 +1,78 @@
+package com.want.movie.ui.util;
+
+
+import android.content.Context;
+import android.graphics.*;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.widget.FrameLayout;
+
+import com.want.movie.R;
+
+public class RoundedBitmapView extends FrameLayout {
+    private float CORNER_RADIUS;
+
+    private Bitmap maskBitmap;
+    private Paint paint, maskPaint;
+    private float cornerRadius;
+
+    public RoundedBitmapView(Context context) {
+        super(context);
+        init(context, null, 0);
+    }
+
+    public RoundedBitmapView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    public RoundedBitmapView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context, attrs, defStyle);
+    }
+
+    private void init(Context context, AttributeSet attrs, int defStyle) {
+        CORNER_RADIUS = context.getResources().getDimension(R.dimen.filter_radius);
+
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        cornerRadius = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, CORNER_RADIUS, metrics);
+
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        maskPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
+        maskPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+
+        setWillNotDraw(false);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        Bitmap offscreenBitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas offscreenCanvas = new Canvas(offscreenBitmap);
+
+        super.draw(offscreenCanvas);
+
+        if (maskBitmap == null) {
+            maskBitmap = createMask(canvas.getWidth(), canvas.getHeight());
+        }
+
+        offscreenCanvas.drawBitmap(maskBitmap, 0f, 0f, maskPaint);
+        canvas.drawBitmap(offscreenBitmap, 0f, 0f, paint);
+    }
+
+    private Bitmap createMask(int width, int height) {
+        Bitmap mask = Bitmap.createBitmap(width, height, Bitmap.Config.ALPHA_8);
+        Canvas canvas = new Canvas(mask);
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.WHITE);
+
+        canvas.drawRect(0, 0, width, height, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        canvas.drawRoundRect(new RectF(0, 0, width, height), cornerRadius, cornerRadius, paint);
+
+        return mask;
+    }
+}
